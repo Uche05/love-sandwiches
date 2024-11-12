efe924da3bc1ead9e19793d1bd495585b7c2d81f
Uchechukwu Kpadeuwa <uchekpadeuwa@gmail.com>
2024-11-12 19:22:05 +0000

    try to get last 5 entries of sales

--- run.py
+++ run.py
@@ -82,7 +82,21 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
-
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales sheet,
+    collecting the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    #column = sales.col_values(3)
+    #print(column)
+    
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column)
+    pprint(columns)
 
 def main():
     """
@@ -97,4 +111,5 @@ def main():
     
     
 print("Welcome to love sandwiches data automation!")
-main()
+#main()
+get_last_5_entries_sales()
